57372cf5acd2b8433e3eb852f65ab1573105ec7a
khattak-k <fahadkhattak5@gmail.com>
2021-06-25 19:14:59 +0500

    final

--- app.js
+++ app.js
@@ -1,31 +1,54 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/api/users");
+var pslTeamsRouter = require("./routes/api/pslTeams");
 
 
+var config = require("config");
+var cors = require('cors');
+
+var app = express();
+app.use(cors());
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/pslTeams",pslTeamsRouter);
 
 
-    this.Add = function(){
-            var team1 = document.getElementById('team1').value;
-            var team2 = document.getElementById('team2').value;
-            var city = document.getElementById('city').value;
-            var date = document.getElementById('date').value;
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
-            var macthesInfo = document.getElementById("macthesInfo");
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-            newmatch = team1+'vs'+team2+city+date;
-            var matchtext = document.createTextNode(newmatch);
-
-            var Newlist = document.createElement('li')
-            Newlist.appendChild(matchtext);
-            macthesInfo.appendChild(Newlist);
-
-
-            
-    };
-
-    this.Delete = function(item){
-
-    };
-
-    this.Count = function(data){
-
-    };
-
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+mongoose
+  .connect(config.get("db"), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Mongo...."))
+  .catch((error) => console.log(error.message));
+module.exports = app;
